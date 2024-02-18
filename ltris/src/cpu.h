8766839c8e5c0408d212b08bc32d16ab99cdcf65
kulkanie <kulkanie@b357a508-0cf7-4c0e-b265-6d29fad4a9a1>
2024-02-18 16:16:13 +0000

    fixed indention for cpu.c/h

--- ltris/src/cpu.h
+++ ltris/src/cpu.h
@@ -26,17 +26,17 @@ original_bowl: 0 - empty
 bowl:          2 - inserted block
                3 - removed line 
 ====================================================================
-*/
+ */
 typedef struct {
-    int aggr; /* if playing aggressive for multiplayer action we punish single lines */
-    Block_Mask *original_block, *original_preview; /* the two blocks tested */
-    int bowl_w, bowl_h;
-    int original_bowl[BOWL_WIDTH][BOWL_HEIGHT]; /* set by bowl before calling cpu_analyze_data() */
-    int backup_bowl[BOWL_WIDTH][BOWL_HEIGHT]; /* this bowl stores the bowl information by cpu_backup_bowl() 
+	int aggr; /* if playing aggressive for multiplayer action we punish single lines */
+	Block_Mask *original_block, *original_preview; /* the two blocks tested */
+	int bowl_w, bowl_h;
+	int original_bowl[BOWL_WIDTH][BOWL_HEIGHT]; /* set by bowl before calling cpu_analyze_data() */
+	int backup_bowl[BOWL_WIDTH][BOWL_HEIGHT]; /* this bowl stores the bowl information by cpu_backup_bowl()
                                                  and restores by cpu_restore_bowl() */
-    int bowl[BOWL_WIDTH][BOWL_HEIGHT]; /* this bowl is used to actually compute stuff */
-    Block_Mask *block; /* actual block tested */
-    int dest_x, dest_y, dest_rot, dest_score; /* this is the CPU result for this data */
+	int bowl[BOWL_WIDTH][BOWL_HEIGHT]; /* this bowl is used to actually compute stuff */
+	Block_Mask *block; /* actual block tested */
+	int dest_x, dest_y, dest_rot, dest_score; /* this is the CPU result for this data */
 } CPU_Data;
 
 /*
@@ -44,7 +44,7 @@ typedef struct {
 Analyze situation and set CPU_Data::dest_x and CPU_Data::dest_rot
 which is used by bowl to move the block.
 ====================================================================
-*/
+ */
 void cpu_analyze_data( CPU_Data *cpu_data );
 
 #endif
